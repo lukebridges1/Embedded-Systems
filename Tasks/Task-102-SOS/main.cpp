acefa99270680a728707fc4f5fddc359b3d1a985
lukebridges1 <146337665+lukebridges1@users.noreply.github.com>
2023-10-05 11:44:17 +0100

    Update main.cpp

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -23,17 +23,17 @@ int main()
         //On for 500ms
         greenLED = 1;
         buzz.playTone("C");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        wait_us(WAIT_TIME_MS * 70);  //500ms
 
 
         //Off for 500ms
         greenLED = 0;
         buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        wait_us(WAIT_TIME_MS * 120);  //500ms
 
         //Pause
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 1000);
+        wait_us(WAIT_TIME_MS * 60);
 
     }
 }
