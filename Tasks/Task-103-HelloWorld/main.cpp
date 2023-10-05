49300afc5e073e275554e94b5ae4e1ab8152a78d
lukebridges1 <146337665+lukebridges1@users.noreply.github.com>
2023-10-05 12:29:55 +0100

    Changed "Hello World" to "wassup playuh"

--- Tasks/Task-103-HelloWorld/main.cpp
+++ Tasks/Task-103-HelloWorld/main.cpp
@@ -13,7 +13,7 @@ LCD_16X2_DISPLAY lcd;
 int main()
 {
     // Write to serial terminal on host PC
-    printf("Hello World\n");
+    printf("wassup playuh\n");
 
     // Clear the LCD screen
     lcd.cls();
@@ -27,7 +27,7 @@ int main()
     ledBlue = 0;
 
     // Write to LCD using the LCD_16X2_DISPLAY driver class
-    lcd.printf("Hello World");
+    lcd.printf("wassup playuh");
     lcd.locate(1, 0);   //Row 1, Col 0
     lcd.printf("ELEC143");
 
