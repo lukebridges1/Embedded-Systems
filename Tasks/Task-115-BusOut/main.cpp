70cdf70844e92a219281c2f592d63ecfdb99974c
lukebridges1 <146337665+lukebridges1@users.noreply.github.com>
2023-11-30 15:48:04 +0000

    Update main.cpp

--- Tasks/Task-115-BusOut/main.cpp
+++ Tasks/Task-115-BusOut/main.cpp
@@ -1,29 +1,29 @@
 #include "mbed.h"
-#define WAIT_TIME_MS 1000 
+#define WAIT_TIME_MS 2500 
 
 
 // DigitalOut redLED(PC_2,1);
 // DigitalOut yellowLED(PC_3,1);
 // DigitalOut greenLED(PC_6,1);
 
-BusOut lights(PC_2, PC_3, PC_6);
+BusOut lights(PC_2, PC_3, PC_6, PC_0, PC_7, PC_14);
 //BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 
 int main()
 {
     //All OFF
-    lights = 0b000;
+    lights = 0b111111;
 
     while (true)
     {
-        int count = 0;
-        while (count <= 7) 
+        int count = 7;
+        while (count >= 0) 
         {
             printf("count = %d\n", count);
             lights = count;
             wait_us(1000000);
 
-            count = count + 1;
+            count = count - 1;
         }
     }
 }
