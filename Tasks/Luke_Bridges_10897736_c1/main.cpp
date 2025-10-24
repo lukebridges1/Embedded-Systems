69856c63c512b9d37880cc8f9c0097def0b91c81
lukebridges1 <146337665+lukebridges1@users.noreply.github.com>
2025-10-24 15:50:13 +0100

    Create main.cpp

---
+++ Tasks/Luke_Bridges_10897736_c1/main.cpp
@@ -0,0 +1,40 @@
+#include "mbed.h"
+#include <cstring>
+
+class Rectangle {
+    private:
+    float length; 
+    float width;
+
+    public:
+// constructor to initialize length and width 
+    Rectangle (float l, float w) {
+        length = l;
+        width = w;
+    }
+// calculate area
+    float Area() {
+        return length * width;
+    }
+
+// calculate perimeter
+    float Perimeter() {
+        return 2 * (length + width);
+    }
+};
+
+
+
+
+// main() runs in its own thread in the OS
+int main()
+{
+    printf("Program Starting...\n");
+// create rectangle object 
+    Rectangle rec1(6,4);
+
+// output area and perimeter 
+    printf("Rectangle Area: %f\n", rec1.Area());
+    printf("Rectangle Perimeter: %.2f\n", rec1.Perimeter());
+}
+
